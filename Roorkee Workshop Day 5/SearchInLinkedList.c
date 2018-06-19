d48b1551ef375094e4afc6d074be43d5ac533d1a
Arjun <arjunmalik18@gmail.com>
2018-06-19 10:02:57 +0530

    Day 5 Problems

---
+++ Roorkee Workshop Day 5/SearchInLinkedList.c
@@ -0,0 +1,62 @@
+// Iterative C program to search an element in linked list
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h
+
+/* Link list node */
+struct Node
+{
+    int key;
+    struct Node* next;
+};
+
+/* Given a reference (pointer to pointer) to the head
+  of a list and an int, push a new node on the front
+  of the list. */
+void push(struct Node** head_ref, int new_key)
+{
+    /* allocate node */
+    struct Node* new_node =
+            (struct Node*) malloc(sizeof(struct Node));
+
+    /* put in the key  */
+    new_node->key  = new_key;
+
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);
+
+    /* move the head to point to the new node */
+    (*head_ref)    = new_node;
+}
+
+/* Checks whether the value x is present in linked list */
+bool search(struct Node* head, int x)
+{
+    struct Node* current = head;  // Initialize current
+    while (current != NULL)
+    {
+        if (current->key == x)
+            return true;
+        current = current->next;
+    }
+    return false;
+}
+
+/* Driver program to test count function*/
+int main()
+{
+    /* Start with the empty list */
+    struct Node* head = NULL;
+    int x = 21;
+
+    /* Use push() to construct below list
+     14->21->11->30->10  */
+    push(&head, 10);
+    push(&head, 30);
+    push(&head, 11);
+    push(&head, 21);
+    push(&head, 14);
+
+    search(head, 21)? printf("Yes") : printf("No");
+    return 0;
+}
